26566f9c1e1954ceed6b97efedbec684ffd7c4b4
mattyb709 <matthew.builes8@gmail.com>
2025-04-01 02:13:30 -0500

    cleaned up data folder

--- architecture.py
+++ architecture.py
@@ -3,11 +3,19 @@ import numpy as np
 class nn:
     def __init__(self):
         self.layer1 = LinearLayer(28*28, 256)
+        self.act1 = ReLU()
         self.layer2 = LinearLayer(256, 128)
+        self.act2 = ReLU()
         self.layer3 = LinearLayer(128, 10)
-        self.act = ReLU()
-        
+    
+    def forward(self, x):
+        x = self.layer1.forward(x)
+        x = self.act1.forward(x)
+        x = self.layer2.forward(x)
+        x = self.act2.forward(x)
+        x = self.layer3.forward(x)
 
+        
 class LinearLayer:
 
     def __init__(self, in_features, out_features):
@@ -16,6 +24,7 @@ class LinearLayer:
         self.bias = np.zeros(out_features)
 
     def forward(self, x):
+        # TODO store activations for backward pass
         x = np.matmul(self.weights, x)
         x = x + self.bias
 
@@ -24,7 +33,7 @@ class ReLU:
     def forward(x):
         return np.maximum(0, x)
     
-    
+
         
 x = np.random.randn(5,6)
 print(x)
